06e1556a70e699eff03e0c2580db6ebce09929f3
Robert Celestino <panda@Roberts-MacBook-Air.local>
2020-01-22 19:14:37 -0500

    hi

---
+++ src/components/ColorWorkhop/ColorBox.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ColorBox(props) {
+  return <div>color slider</div>;
+}
+
+export default ColorBox;
---
+++ src/components/ColorWorkhop/ColorSlider.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ColorSlider(props) {
+  return <div>color slider</div>;
+}
+
+export default ColorSlider;
