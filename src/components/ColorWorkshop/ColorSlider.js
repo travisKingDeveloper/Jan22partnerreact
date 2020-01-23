dc829d999eb1a59f8a9add67b29284eb4e8213cc
Travis King <travis.king.developer@gmail.com>
2020-01-22 20:16:34 -0500

    Have sliders, need them to do something

--- src/components/ColorWorkshop/ColorSlider.js
+++ src/components/ColorWorkshop/ColorSlider.js
@@ -1,32 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+
+const defaultState = {
+  red: 0,
+  blue: 0,
+  green: 255,
+}
 
 function ColorSlider(props) {
+  const [colorValue, setColorValue] = useState(defaultState)
+
+  const changeSetColorValue = (e) => {
+    const value = e.target.value
+
+    return setColorValue(value)
+  }
+
   return (
-    <div className="color-card color-slider">
+    <div className="color-card">
+      <div className="color-slider">
       <input
         type="range"
-        min="1"
-        max="100"
-        value="50"
-        class="slider"
-        id="myRange"
+        onChange={changeSetColorValue}
+        min="0"
+        max="255"
+        defaultValue={colorValue.red}
+        className="slider"
+        id="red"
       ></input>
       <input
         type="range"
-        min="1"
-        max="100"
-        value="50"
-        class="slider"
-        id="myRange"
+        onChange={changeSetColorValue}
+        min="0"
+        max="255"
+        defaultValue={colorValue.blue}
+        className="slider"
+        id="blue"
       ></input>
       <input
         type="range"
-        min="1"
-        max="100"
-        value="50"
-        class="slider"
-        id="myRange"
+        onChange={changeSetColorValue}
+        min="0"
+        max="255"
+        defaultValue={colorValue.green}
+        className="slider"
+        id="green"
       ></input>
+      </div>
     </div>
   );
 }
